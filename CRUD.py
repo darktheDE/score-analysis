803325cdc2f1d49e05cebcc692ad75c1a8faed8d
DanhTrannn <tranthanhdanhktvn2005d@gmail.com>
2024-10-23 21:22:15 +0700

    update function for CRUD

--- CRUD.py
+++ CRUD.py
@@ -11,7 +11,7 @@ class crud:
             writer = csv.writer(csv_file)
             row = [sbd] + scores_list
             writer.writerow(row)
-    def read_score(self):
+    def read_score(self): 
         scores  = []
         with open(self.file_path, mode = 'r', newline='') as csv_file:
             reader = csv.reader(csv_file)
@@ -26,4 +26,30 @@ class crud:
 
                 scores.append(sbd,score_dict)
         return scores
-    
+    def update_score(self, sbd, subject, new_score): # cap nhat diem cua mot mon hoc cu the
+        data = []
+        with open(self.file_path, mode = 'r', newline = '') as csv_file:
+            reader = csv.reader(csv_file) #csv.reader chi tra ve iterator 
+            data = list(reader)
+        with open(self.file_path, mode = 'w', newline = '') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(data[0]) # ghi vao dong chua tieu de
+            for row in data[1:]:
+                if row[0] == sbd: # neu row[0] bang sbd can cap nhat diem thi cap nhat lai diem
+                    if subject in self.subjects:
+                        index_update = self.subjects.index(subject) + 1
+                        row[index_update] = new_score
+                        writer.writerow(row)
+                else:
+                    writer.writerow(row)
+    def delete_score(self, sbd): # xoa hoc sinh theo sbd
+        data = []
+        with open(self.file_path, mode = 'r', newline = '') as csv_file: # doc du lieu du file vao
+            reader = csv.reader(csv_file)
+            data = list(reader)
+        with open(self.file_path, mode = 'w', newline = '') as csv_file: # ghi de du lieu vao trong file
+            writer = csv.writer(csv_file)
+            writer.writerow(data[0]) # ghi vao tieu de
+            for row in data[1:]:
+                if row[0] != sbd:
+                    writer.writerow(row)
