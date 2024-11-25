b7e88be941f86e2c340f2537e8feefe24d536ed7
leekhoa0409 <leekhoa0409@gmail.com>
2024-11-25 11:12:25 +0700

    rao loi input

--- CRUD.py
+++ CRUD.py
@@ -1,20 +1,9 @@
 import csv
 
-
 class CRUD:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.subjects = [
-            "toan",
-            "ngu_van",
-            "ngoai_ngu",
-            "vat_li",
-            "hoa_hoc",
-            "sinh_hoc",
-            "lich_su",
-            "dia_li",
-            "gdcd",
-        ]
+        self.subjects = ["toan", "ngu_van", "ngoai_ngu", "vat_li", "hoa_hoc", "sinh_hoc", "lich_su", "dia_li", "gdcd"]
         self.province_code = {"02": "TP. Hồ Chí Minh"}
         self.groups = {
             "A00": ["toan", "vat_li", "hoa_hoc"],
@@ -22,6 +11,9 @@ class CRUD:
             "B00": ["toan", "hoa_hoc", "sinh_hoc"],
             "C00": ["ngu_van", "lich_su", "dia_li"],
             "D01": ["ngu_van", "toan", "ngoai_ngu"],
+        }
+        self.convert = {"toan":"toán", "ngu_van" : "ngữ văn", "ngoai_ngu": "ngoại ngữ", "vat_li":"vật lí", "hoa_hoc": "hóa học",
+                        "sinh_hoc":"sinh học", "lich_su":"lịch sử","dia_li": "địa lí", "gdcd": "giáo dục công dân"
         }
 
     def add_score(self, sbd, scores):
@@ -31,28 +23,26 @@ class CRUD:
         Args:
             sbd(string): Số báo danh cho thí sinh cần thêm
             scores(dictionary): Chứa các item với key là tên môn thi và value là điểm thi
-
-        Returns:
-            (none)
         """
         scores_list = [scores.get(subject) for subject in self.subjects]
-        with open(self.file_path, mode="a", newline="") as csv_file:
+        with open(self.file_path, mode = 'a', newline='') as csv_file:
             writer = csv.writer(csv_file)
             row = [sbd] + scores_list
             writer.writerow(row)
 
-    def read_score(self):
+
+    def read_score(self): 
         """
         Hàm đọc dữ liệu điểm thi từ file csv
 
         Args:
             (none)
-
+        
         Returns:
             list: chứa danh sách các dòng dữ liệu được đọc từ file
         """
-        scores = []
-        with open(self.file_path, mode="r", newline="") as csv_file:
+        scores  = []
+        with open(self.file_path, mode = 'r', newline='') as csv_file:
             reader = csv.reader(csv_file)
             next(reader)
             for row in reader:
@@ -61,9 +51,10 @@ class CRUD:
                 j = 1
                 for subject in self.subjects:
                     score_dict[subject] = row[j]
-                    j += 1
+                    j+=1
                 scores.append((sbd, score_dict))
         return scores
+    
 
     def update_score(self, sbd, subject, new_score):
         """
@@ -73,15 +64,12 @@ class CRUD:
             sbd(string): Số báo danh của thí sinh cần cập nhật điểm thi
             subject(string): Tên môn học cần sửa đổi điểm
             new_score(float): Điểm thi mới cần cập nhật cho môn học đó
-
-        Returns:
-            (none)
         """
         data = []
-        with open(self.file_path, mode="r", newline="") as csv_file:
+        with open(self.file_path, mode = 'r', newline = '') as csv_file:
             reader = csv.reader(csv_file)
             data = list(reader)
-        with open(self.file_path, mode="w", newline="") as csv_file:
+        with open(self.file_path, mode = 'w', newline = '') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(data[0])
             for row in data[1:]:
@@ -93,96 +81,41 @@ class CRUD:
                 else:
                     writer.writerow(row)
 
+
     def delete_score(self, sbd):
         """
         Hàm xoá toàn bộ điểm thi dựa vào số báo danh của thí sinh đó
 
         Args:
             sbd(string): Số báo danh của thí sinh cần xoá điểm thi
-
-        Returns:
-            (none)
         """
         data = []
-        with open(self.file_path, mode="r", newline="") as csv_file:
+        with open(self.file_path, mode = 'r', newline = '') as csv_file:
             reader = csv.reader(csv_file)
             data = list(reader)
-        with open(self.file_path, mode="w", newline="") as csv_file:
+        with open(self.file_path, mode = 'w', newline = '') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(data[0])
             for row in data[1:]:
                 if row[0] != sbd:
                     writer.writerow(row)
 
-    def avg_score(self):
-        """
-        Hàm tính điểm trung bình cho từng tỉnh thành
-
-        Args:
-            (none)
-
-        Returns:
-            (none)
-        """
-        with open(self.file_path, mode="r", newline="") as csv_file:
-            reader = csv.reader(csv_file)
-            # Bỏ qua dòng tiêu đề
-            next(reader)
-            # Tạo list data là các dòng dữ liệu
-            data = [row for row in reader]
-
-        # Tạo dict để lưu kết quả cho mỗi items với key tỉnh và value là đtb của tỉnh
-        province_scores = {}
-
-        for row in data:
-            # Lấy mã tỉnh của từng thí sinh
-            province_code_key = row[0][:2]
-            # Xác định tỉnh thông qua mã tỉnh
-            province = self.province_code.get(province_code_key)
-            score = 0
-            count = 0
-
-            for i in range(1, 10):
-                str(row[i])
-                # Nếu điểm thi môn i khác rỗng -> tính tổng điểm / số lượng môn
-                if row[i] != "":
-                    score += float(row[i])
-                    count += 1
-
-            if count > 0:
-                score /= count
-            # Nếu tỉnh không nằm trong ds điểm từng tỉnh -> tạo value mới cho tỉnh đó là một list điểm rỗng
-            if province not in province_scores:
-                province_scores[province] = []
-            # Nếu đã được tạo danh sách đtb của từng thí sinh tỉnh đó -> tiến hành thêm đtb của thí sinh kế tiếp
-            province_scores[province].append(score)
-
-        # Tạo dict chứa các item với key là tên tình và value là đtb của từng tỉnh
-        avg_by_province = {
-            province: sum(scores) / len(scores)
-            for province, scores in province_scores.items()
-        }
-        print("\nĐiểm trung bình từng tỉnh thành:")
-        for province, avg_score in avg_by_province.items():
-            print(f"{province}: {avg_score:.2f}")
 
     def find_top_scorer_per_group(self):
         """
-        Hàm tìm thủ khoa toàn quốc cho từng khối thi
+        Hàm tìm thủ khoa TPHCM cho từng khối thi
 
         Args:
             (none)
-
-        Returns:
-            (none)
         """
-        with open(self.file_path, mode="r", newline="") as csv_file:
+        with open(self.file_path, mode='r', newline='') as csv_file:
             reader = csv.reader(csv_file)
             # Lấy hàng tiêu đề và trỏ qua dòng tiếp theo
             header = next(reader)
             header = list(header)
             # Tạo list data chứa các dòng dữ liệu
             data = [row for row in reader]
+
 
         # Duyệt qua từng khối thi
         for group, group_subjects in self.groups.items():
@@ -192,15 +125,15 @@ class CRUD:
             max_score = -1
             # Dict chứa các item với key là môn thi và value là điểm thi của môn đó
             top_scores = {}
-
+            
             for row in data:
                 total_score = 0
                 count_score = 0
                 # Duyệt qua các môn thi trong khối thi đó
                 for subject in group_subjects:
-                    for j in range(len(header)):
+                    for j in range (len(header)):
                         # Nếu môn thi đó có trong header và điểm thi môn đó của thi sinh khác rỗng
-                        if subject == header[j] and row[j] != "":
+                        if subject == header[j] and row[j] != '':
                             total_score += float(row[j])
                             count_score += 1
 
@@ -209,18 +142,93 @@ class CRUD:
                     max_score = total_score
                     top_scorer = row[0]
                     for subject in group_subjects:
-                        if subject in header and row[header.index(subject) != ""]:
+                        if subject in header and row[header.index(subject) != '']:
                             # Thêm môn đó vào dict chứa các item với key là điểm thi 1 môn trong khối và value là điểm thi môn đố
                             top_scores[subject] = row[header.index(subject)]
 
             # Nếu tồn tại thủ khoa khối đó -> In ra màn hình
             if top_scorer:
-                scores_str = ", ".join(
-                    [
-                        f"{subject}: {top_scores.get(subject, 'N/A')}"
-                        for subject in group_subjects
-                    ]
-                )
-                print(
-                    f"Thủ khoa khối {group}: SBD: {top_scorer}, Tổng điểm: {max_score:.2f}, Điểm chi tiết: {scores_str}"
-                )
+                scores_str = ', '.join([f"{subject}: {top_scores.get(subject, 'N/A')}" for subject in group_subjects])
+                print(f"Thủ khoa khối {group}: SBD: {top_scorer}, Tổng điểm: {max_score:.2f}, Điểm chi tiết: {scores_str}")
+                
+    def sort_by_subject(self, subject, reversed):
+        """
+        Hàm sắp xếp danh sách thí sinh theo điểm tăng dần của một môn học và in ra toàn bộ thông tin
+
+        Args:
+            subject (string): Tên môn học để sắp xếp
+            reversed(boolean): Biến để xác định sắp xếp tăng dần hay giảm dần
+        """
+        if subject not in self.subjects:
+            print(f"Môn học '{subject}' không hợp lệ. Vui lòng chọn trong danh sách: {self.subjects}")
+            return
+
+        with open(self.file_path, mode='r', newline='') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+            data = list(reader)
+
+        # Lấy index của môn học cần sắp xếp
+        subject_index = self.subjects.index(subject) + 1
+
+        sorted_data = sorted(data, key=lambda row: float(row[subject_index]) if row[subject_index] != '' else 0, reverse=reversed)
+
+        # In kết quả sắp xếp ra màn hình
+        print(f"\nDanh sách sắp xếp theo điểm môn '{self.convert[subject]}:")
+        for row in sorted_data:
+            print(row)
+    
+    def find_by_sbd(self, sbd_target):
+        """
+        Hàm tìm kiếm thông tin thí sinh theo số báo danh
+
+        Args:
+            sbd_target(string): Truyền vào số báo danh của thí sinh cần tìm kiếm
+        """
+        with open(self.file_path, mode='r', newline='') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+            data = list(reader)
+        
+        for row in data:
+            if row[0] == sbd_target:
+                print("\nThông tin của thí sinh cần tìm kiếm: ")
+                print(row)
+                return
+        
+        print("Không tìm thấy thông tin thí sinh với số báo danh:", sbd_target)
+
+    
+    def find(self, subject, score):
+        """
+        Hàm tìm kiếm thông tin các thí sinh thi môn có điểm thi bằng với điểm thi cần tìm
+
+        Args:
+            subject(string): Môn thi cần tìm:
+            score(float): Điểm thi cần tìm
+        """
+        if subject not in self.subjects:
+            print(f"Môn học '{subject}' không hợp lệ. Vui lòng chọn trong danh sách: {self.subjects}")
+            return
+        with open(self.file_path, mode='r', newline='') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+            data = list(reader)
+
+        subject_index = self.subjects.index(subject) + 1
+        # Format lại điểm thi các môn là rỗng
+        for row in data:
+            for subject in self.subjects:
+                if row[self.subjects.index(subject) + 1] == '':
+                    row[self.subjects.index(subject) + 1] = 0
+
+        print("Thông tin các thí sinh thi môn", self.convert[subject], "có điểm", score, "là:")
+        check = True
+        for row in data:
+            if row[subject_index] != '':
+                if float(row[subject_index]) == score:
+                    check = False
+                    print(row)
+        
+        if check == True:
+            print("Không tìm thấy thông tin thí sinh thi môn", self.convert[subject], "có điểm", score)
--- Main.py
+++ Main.py
@@ -1,183 +1,223 @@
 import CRUD
-
+import subprocess
+import os
 file_path1 = "diem2023.csv"
 file_path2 = "diem2024.csv"
 crud_instance_2023 = CRUD.CRUD(file_path1)
 crud_instance_2024 = CRUD.CRUD(file_path2)
 
+def isFloat(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-def add_new_score():
+def menu(crud):
     choose = "-1"
-    while choose != "0":
-        print("\nMENU OPTIONS")
-        print("1. Thêm điểm thi cho thí sinh mới vào dữ liệu năm 2023")
-        print("2. Thêm điểm thi cho thí sinh mới vào dữ liệu năm 2024")
-        print("0. Trở lại menu chính")
+    while True:
+        print("\nSUB-MENU OPTIONS")
+        print("1. Nhập điểm thi cho một thí sinh mới")
+        print("2. Đọc dữ liệu điểm thi từ file dữ liệu và xuất ra màn hình")
+        print("3. Cập nhật điểm thi thông qua số báo danh của một thí sinh")
+        print("4. Xóa dữ liệu của một thí sinh thông qua số báo danh")
+        print("5. Xuất ra màn hình thủ khoa các khối thi ở TPHCM")
+        print("6. Sắp xếp điểm tăng dần/giảm dần theo môn học")
+        print("7. Tìm thông tin điểm thi của thí sinh theo số báo danh")
+        print("8. Tìm thông tin thí sinh theo môn thi và điểm thi")
+        print("0. Về lại menu chính")
 
         choose = input("\nNhập lựa chọn của bạn: ")
-
+        
         if choose == "1":
-            sbd_input = input("Nhập số báo danh: ")
+            sbd_input = ""
+            while len(sbd_input) != 8 or not sbd_input.isdigit:
+                sbd_input = input("Nhập số báo danh: ")
+                if len(sbd_input) != 8 or not sbd_input.isdigit:
+                    print("Số báo danh không hợp lệ, vui lòng nhập lại theo format của bộ giáo dục!")
+
             scores_input = {}
-            for subject in crud_instance_2023.subjects:
-                score = ""
-                check_number = True
-                check_string = True
+            for subject in crud.subjects:
+                score_input = ""
+                check_num = True
+                check_abc = True
                 score_float = 0
-                while check_number or check_string:
-                    score = input(
-                        f"Nhập điểm thi môn {subject} (-1 nếu không thi môn đó): "
-                    )
-                    check_number = False
-                    check_string = False
-                    for i in range(len(score)):
-                        if score[0] == "-":
+
+                while check_num or check_abc:
+                    score_input = input(f"Nhập điểm thi môn {crud.convert[subject]} (-1 nếu không thi môn đó): ")
+                    check_num = False
+                    check_abc = False
+                    for i in range(len(score_input)):
+                        if score_input[0] == '-':
                             continue
-                        if score[i].isdigit() == False:
-                            check_string = True
+                        if isFloat(score_input) == False:
+                            check_abc = True
                             print("Có lỗi xảy ra, vui lòng nhập điểm là chữ số!")
                             break
-
-                    if check_string == False:
-                        score_float = float(score)
+                    
+                    if check_abc == False:
+                        score_float = float(score_input)
                         if score_float < -1 or score_float > 10:
                             print("Điểm thi không hợp lệ, vui lòng nhập lại!")
-                            check_number = True
+                            check_num = True
 
                 if score_float == -1:
-                    scores_input[subject] = ""
+                    scores_input[subject] = ''
                 else:
                     scores_input[subject] = score_float
-
-            crud_instance_2023.add_score(sbd_input, scores_input)
-            print("Thêm dữ liệu cho thí sinh mới năm 2023 thành công!")
+                
+            crud.add_score(sbd_input, scores_input)
+            print("Thêm dữ liệu cho thí sinh mới thành công!")
 
         elif choose == "2":
-            sbd_input = input("Nhập số báo danh: ")
-            scores_input = {}
-            for subject in crud_instance_2024.subjects:
-                score = ""
-                check_number = True
-                check_string = True
-                score_float = 0
-                while check_number or check_string:
-                    score = input(
-                        f"Nhập điểm thi môn {subject} (-1 nếu không thi môn đó): "
-                    )
-                    check_number = False
-                    check_string = False
-                    for i in range(len(score)):
-                        if score[0] == "-":
-                            continue
-                        if score[i].isdigit() == False:
-                            check_string = True
-                            print("Có lỗi xảy ra, vui lòng nhập điểm là chữ số!")
-                            break
+            temp_list = crud.read_score()
+            for row in temp_list:
+                print(row)
+            print("\nDữ liệu trong file đã được đọc thành công!")
 
-                    if check_string == False:
-                        score_float = float(score)
-                        if score_float < -1 or score_float > 10:
-                            print("Điểm thi không hợp lệ, vui lòng nhập lại!")
-                            check_number = True
+        elif choose == "3":
+            sbd_input = ""
+            subject_input = ""
+            score_float = 0
+            check = True
+            while check:
+                sbd_input = input("Nhập số báo danh: ")
+                temp_list = crud.read_score()
+                for row in temp_list:
+                    if sbd_input == row[0]:
+                        check = False
+                        break
+                    
+                if check == False:
+                    subject_input = ''
+                    while subject_input not in crud.subjects:
+                        subject_input = input("Nhập tên môn học cần cập nhật: ")
+                        if subject_input not in crud.subjects:
+                            print("Không tìm thấy môn học này trong các môn thi, vui lòng thử lại theo danh sách các môn\n", crud.subjects)
 
-                if score_float == -1:
-                    scores_input[subject] = ""
+                    score_input = ""
+                    check_num = True
+                    check_abc = True
+                    while check_num or check_abc:
+                        score_input = input(f"Nhập điểm thi môn {crud.convert[subject_input]} cần thay đổi: ")
+                        check_num = False
+                        check_abc = False
+                        for i in range(len(score_input)):
+                            if score_input[0] == '-':
+                                continue
+                            if isFloat(score_input) == False:
+                                check_abc = True
+                                print("Có lỗi xảy ra, vui lòng nhập điểm là chữ số!")
+                                break
+                    
+                        if check_abc == False:
+                            score_float = float(score_input)
+                            if score_float < -1 or score_float > 10:
+                                print("Điểm thi không hợp lệ, vui lòng nhập lại!")
+                                check_num = True
+                                
+                if check == True:
+                    print("Số báo danh không hợp lệ, vui lòng nhập lại!")
+
+            crud.update_score(sbd_input, subject_input, score_float)
+            print(f"Cập nhật điểm thi môn {crud.convert[subject_input]} thí sinh {sbd_input} thành công!")
+
+        elif choose == "4":
+            check = True
+            sbd_input = ""
+            while check:
+                sbd_input = input("Nhập số báo danh: ")
+                temp_list = crud.read_score()
+                for row in temp_list:
+                    if sbd_input == row[0]:
+                        check = False
+                        break
+                if check:
+                    print("Số báo danh không hợp lệ, vui lòng nhập lại!")
+
+            print(f"\nXóa thông tin thí sinh {sbd_input} thành công!")
+            crud.delete_score(sbd_input)
+            
+        elif choose == '5':
+            crud.find_top_scorer_per_group()
+
+        elif choose == '6':
+            subject_input = input("Nhập tên môn học cần sắp xếp theo điểm thi: ")
+            user_input = ""
+            isDescending = True
+            while True:
+                user_input = input("Tăng dần (A) / giảm dần (D): ")
+                if user_input == "A" or user_input == "D" or user_input == 'a' or user_input == 'd':
+                    break
                 else:
-                    scores_input[subject] = score_float
+                    print("Lựa chọn không hợp lệ, vui lòng lựa thử lại!")
+            
+            if user_input == "A" or user_input == 'a':
+                isDescending = False
 
-            crud_instance_2024.add_score(sbd_input, scores_input)
-            print("Thêm dữ liệu cho thí sinh mới năm 2024 thành công!")
+            crud.sort_by_subject(subject_input, isDescending)
 
+        elif choose == '7':
+            sbd_input = ""
+            while len(sbd_input) != 8 or not sbd_input.isdigit:
+                sbd_input = input("Nhập số báo danh cần tìm kiếm: ")
+                if len(sbd_input) != 8 or not sbd_input.isdigit:
+                    print("Số báo danh không hợp lệ, vui lòng nhập lại theo format của bộ giáo dục!")
+                                
+            crud.find_by_sbd(sbd_input)
+
+        elif choose == '8':
+            subject_input = ''
+            while subject_input not in crud.subjects:
+                subject_input = input("Nhập môn học cần tìm kiếm: ")
+                if subject_input not in crud.subjects:
+                    print("Không tìm thấy môn học này trong các môn thi, vui lòng thử lại theo danh sách các môn\n", crud.subjects)
+            check_num = True
+            check_abc = True
+            score_float = 0
+            while check_num or check_abc:
+                check_num = False
+                check_abc = False
+                score_input = input("Nhập điểm thi môn học cần tìm kiếm: ")
+                if score_input[0] == '-':
+                    print("\nVui lòng nhập điểm thi không âm!")
+                    check_num = True
+                else:
+                    for i in range(len(score_input)):
+                        if isFloat(score_input) == False:
+                            check_abc = True
+                            print("\nVui lòng nhập điểm thi là chữ số!")
+                            break
+                    if check_abc == False:
+                        score_float = float(score_input)
+                        if score_float > 10:
+                            print("\nĐiểm thi không hợp lệ, vui lòng nhập lại!")
+                            check_num = True
+
+            crud.find(subject_input, score_float)
+            
         elif choose == "0":
+            # Kiểm tra hệ điều hành nt = Windows -> thực hiện clrsc trên terminal
+            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
             break
 
         else:
-            print("\nLựa chọn không hợp lệ, vui lòng đọc menu và lựa chọn lại!")
+            print("\nLựa chọn không hợp lệ, vui lòng lựa chọn lại!")
 
 
 choose = "-1"
-while choose != "0":
-    print("\nMENU OPTIONS")
-    print("1. Nhập điểm thi cho một thí sinh mới")
-    print("2. Đọc dữ liệu điểm thi từ file dữ liệu và xuất ra màn hình")
-    print("3. Cập nhật điểm thi thông qua số báo danh của một thí sinh")
-    print("4. Xóa dữ liệu của một thí sinh thông qua số báo danh")
-    print("5. Xuất ra màn hình điểm trung bình từng tỉnh thành")
-    print("6. Xuất ra màn hình thủ khoa cho từng khối")
+while True:
+    print("\nMAIN MENU OPTIONS")
+    print("1. Thực hiện các thao tác trên dữ liệu điểm thi năm 2023")
+    print("2. Thực hiện các thao tác trên dữ liệu điểm thi năm 2024")
     print("0. Thoát chương trình")
-    choose = input("\nNhập lựa chọn của bạn: ")
-
+    choose = input("Nhập lựa chọn của bạn: ")
     if choose == "1":
-        add_new_score()
-
-    # elif choose == 2:
-    #     print("Dữ liệu trong file đã được đọc thành công!")
-    #     temp_list = crud_instance.read_score()
-    #     for row in temp_list:
-    #         print(row)
-
-    # elif choose == 3:
-    #     sbd_input = ""
-    #     subject_input = ""
-    #     score_float = 0
-    #     check = True
-    #     while check:
-    #         sbd_input = input("Nhập số báo danh: ")
-    #         temp_list = crud_instance.read_score()
-    #         for row in temp_list:
-    #             if sbd_input == row[0]:
-    #                 check = False
-    #                 break
-
-    #         if check == False:
-    #             subject_input = input("Nhập tên môn học cần cập nhật: ")
-    #             score = ""
-    #             check_number = True
-    #             check_string = True
-    #             while(check_number or check_string):
-    #                 score = input(f"Nhập điểm thi môn {subject_input} cần thay đổi: ")
-    #                 check_number = False
-    #                 check_string = False
-    #                 for i in range(len(score)):
-    #                     if score[0] == '-':
-    #                         continue
-    #                     if score[i].isdigit() == False:
-    #                         check_string = True
-    #                         print("Có lỗi xảy ra, vui lòng nhập điểm là chữ số!")
-    #                         break
-
-    #                 if check_string == False:
-    #                     score_float = float(score)
-    #                     if score_float < -1 or score_float > 10:
-    #                         print("Điểm thi không hợp lệ, vui lòng nhập lại!")
-    #                         check_number = True
-
-    #         if check == True:
-    #             print("Số báo danh không hợp lệ, vui lòng nhập lại")
-    #     crud_instance.update_score(sbd_input, subject_input, score_float)
-    #     print(f"Cập nhật điểm thi môn {subject_input} thí sinh {sbd_input} thành công!")
-
-    # elif choose == 4:
-    #     check = True
-    #     sbd_input = ""
-    #     while check:
-    #         sbd_input = input("Nhập số báo danh: ")
-    #         temp_list = crud_instance.read_score()
-    #         for row in temp_list:
-    #             if sbd_input == row[0]:
-    #                 check = False
-    #                 break
-    #         if check:
-    #             print("Số báo danh không hợp lệ, vui lòng nhập lại!")
-    #     print(f"Xóa thông tin thí sinh {sbd_input} thành công!")
-    #     crud_instance.delete_score(sbd_input)
-
-    # elif choose == 5:
-    #     crud_instance.avg_score()
-    # elif choose == 6:
-    #     crud_instance.find_top_scorer_per_group()
+        menu(crud_instance_2023)
+    elif choose == "2":
+        menu(crud_instance_2024)
     elif choose == "0":
         break
-
     else:
-        print("\nLựa chọn không hợp lệ, vui lòng đọc menu và lựa chọn lại!")
+        print("\nLựa chọn không hợp lệ, vui lòng nhập lại!")
+
